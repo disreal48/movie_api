df9bbe3a6e5085ef02b7936795bebc98a1ca67a3
disreal48 <disreal48@googlemail.com>
2024-03-09 19:51:55 -0800

    added localhost for testing

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 
 //CORS
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+];
 app.use(
   cors({
     origin: (origin, callback) => {
